6345dddaba36e5cdb89dc934cd0abb926d30e760
Karla <k.rbandala@gmail.com>
2019-03-15 15:33:14 -0600

    Implemented changes in navbar

--- src/LandingHeader/LandingHeader.js
+++ src/LandingHeader/LandingHeader.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button, Col } from 'react-bootstrap';
 import './Header.css';
 
 class LandingHeader extends Component {
     render() {
         return(
             <div id="main-header">
-                <Navbar bg="light" variant="light" fixed = "top" className="rip">
+                <Navbar bg="light" variant="light" fixed = "top" className="main-navbar">
                     <Navbar.Brand href="#home">My Mentor Logo</Navbar.Brand>
                     <Form inline className="md">
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Form.Control type="text" placeholder="Search" className="mr-sm-2"/>
                         <Button variant="outline-info">Search</Button>
                     </Form>
-                    <Nav className="">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav className="navbar-right">
+                        <Nav.Link href="#home">Explora</Nav.Link>
+                        <Nav.Link href="#features">
+                            <Button variant="outline-info">Inicia Sesión</Button>
+                        </Nav.Link>
+                        <Nav.Link href="#pricing">
+                            <Button variant="outline-info">Registrate</Button>
+                        </Nav.Link>
                     </Nav>
                 </Navbar>
             </div>
