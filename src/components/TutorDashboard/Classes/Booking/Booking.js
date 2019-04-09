0f57297cdddc76ec20195b491a639e92920e2c40
Karla Robledo Bandala <bandala@Karlas-MacBook-Pro.local>
2019-04-09 01:39:32 -0500

    implemented functionality for fetching booked and cancelled classes

--- src/components/TutorDashboard/Classes/Booking/Booking.js
+++ src/components/TutorDashboard/Classes/Booking/Booking.js
@@ -1,12 +1,38 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
+import Accepted from './Accepted/Accepted';
+import Cancelled from './Cancelled/Cancelled';
 
 class Bookings extends Component {
     
     render() {
         return(
-            <div>
-
-            </div>
+            <BrowserRouter>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/tutor/bookings/accepted">Aceptadas</Link> 
+                        </li>
+                        <li>
+                            <Link to="/tutor/bookings/cancelled">Canceladas</Link>
+                         </li>
+                    </ul>
+                    <Switch> 
+                        <Route  path="/tutor/bookings/accepted"
+                                    render = {
+                                        (props) => <Accepted {...props} token={this.props.token} />
+                                    }
+                                    exact/> 
+                        <Route  path="/tutor/bookings/cancelled"
+                                    render = {
+                                        (props) => <Cancelled {...props} token={this.props.token} />
+                                    }
+                                    exact/> 
+                    </Switch>
+                    
+                </div>
+            </BrowserRouter>
+            
         );
     }
 }
---
+++ src/components/TutorDashboard/Classes/Booking/BookingCard/BookingCard.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+class BookingCard extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className="booking-card">
+                <p><b>Tutor: </b>{this.props.tutor}</p>
+                <p><b>Class: </b>{this.props.booked_class}</p>
+                <p><b>Student: </b>{this.props.student}</p>
+                <hr></hr>
+            </div>
+        );
+    }
+
+}
+
+export default BookingCard;
