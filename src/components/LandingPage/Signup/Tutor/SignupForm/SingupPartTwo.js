f7bda551d17e0eba75f00db03607c0f1f654d518
Karla Robledo Bandala <bandala@Karlas-MacBook-Pro.local>
2019-04-13 12:56:12 -0500

    Started second part of form integration

--- src/components/LandingPage/Signup/Tutor/SignupForm/SingupPartTwo.js
+++ src/components/LandingPage/Signup/Tutor/SignupForm/SingupPartTwo.js
@@ -2,16 +2,20 @@ import React, {Component} from 'react';
 import { FormGroup, Form, Col } from 'react-bootstrap';
 import { Button, Input } from 'reactstrap';
 class SignupPartTwo extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            didChange: false
+            didChange: false,
+            profilePicture: '',
+            areas: [],
+            description: ''
         };
         this.doSomeTesting = this.doSomeTesting.bind(this);
+        this.handleAreaChange = this.handleAreaChange.bind(this);
     }
-
+    
     render() {
-        console.log(this.state.didChange);
         return(
             <FormGroup>
                 <Form.Label>
@@ -22,38 +26,64 @@ class SignupPartTwo extends Component {
                         name="profile-picture"
                         />
                 
+
                 <Col>
-                    <Form.Label>Área de expertis</Form.Label>
+                    <Form.Label>Área de expertiz</Form.Label>
                 </Col>
                 <Col>
                     <Input  type = "select"
+                            onChange = {this.handleAreaChange}
                             multiple>
-                            <option></option>
-                            <option></option>
-                            <option></option>
-                            <option></option>
-                            <option></option>
-                            <option></option>
+                            <option value='Matematicas'>Matemáticas</option>
+                            <option value="Quimica">Química</option>
+                            <option value="Fisica">Física</option>
+                            <option value="Programacion">Programación</option>
+                            <option value="Electronica">Electrónica</option>
+                            <option value="Economia">Economía</option>
+                            <option value="Estadistica">Estadística</option>
+                            <option value="Ingles">Inglés</option>
+                            <option value="Aleman">Alemán</option>
+                            <option value="Frances">Francés</option>
                     </Input>
                 </Col>
+
                 <Col>
                     <Form.Label>Descripción</Form.Label>
                 </Col>
                 <Col>
                     <Input type="textarea"/>
                 </Col>
-
+ 
                 <Button onClick={this.props.returnToFormOne}>Anterior</Button>
                 <Button onClick={this.doSomeTesting}>Siguiente</Button>
             </FormGroup>
         );
     }
 
+    /****************************************
+    *
+    *   Functions that will handle form input
+    * 
+    *****************************************/
+
     handleSignupSequence = () => {
         this.setState({
             didChange : true
         });
-        console.log('change!');
+    }
+
+    handleAreaChange(event) {
+        const options = event.target.options;
+        var selected = [];
+        for(var i = 0 ; i < options.length; i++) {
+            if(options[i].selected) {
+                selected.push(options[i].value);
+            }
+        }
+        this.setState({
+            areas: selected
+        });
+        console.log(this.state.areas);
     }
 
     doSomeTesting() {
@@ -62,8 +92,6 @@ class SignupPartTwo extends Component {
         console.log(img);
         console.log(aiuda);
     }
-
-
 
 }
 
