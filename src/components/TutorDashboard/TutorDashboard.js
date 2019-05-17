d96c66b051c773e17ef4f29a0f674dda13b9acd6
teredax <gonzisteel@gmail.com>
2019-05-17 12:52:57 -0500

    final details

--- src/components/TutorDashboard/TutorDashboard.js
+++ src/components/TutorDashboard/TutorDashboard.js
@@ -6,7 +6,7 @@ import Notifications from './Notifications/Notifications';
 import Bookings from './Classes/Booking/Booking';
 import MyClasses from './Classes/MyClasses';
 import Profile from './Profile/Profile';
-
+import logo from '../../img/logo.png'
 import { logout } from '../Auth/Auth';
 import Search from '../LandingPage/Search/Search';
 import Reviews from './Reviews/Reviews';
@@ -28,6 +28,13 @@ class TutorDashboard extends Component{
                                 <Route path="/tutors/profile" component={Profile}/>
                                 <Route path="/logout" render={logout}/>
                             </Switch>
+
+  <div className="head_intro_dashboard">
+                        <img id="log_dashboard" src={logo} alt="imagen del logotipo"></img>
+                        <h4>Aprende con </h4>
+                        <h4 id="slogan_1">alguien como tu</h4>
+                    </div>
+
                         </div>
                     </BrowserRouter>
                 </div>
